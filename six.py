9bd3952baa9022086b5f3e9279481428801ae69d
rarch <rarcher496@gmail.com>
2013-07-30 16:21:32 -0700

    added comments

--- six.py
+++ six.py
@@ -3,8 +3,10 @@ from re import search
 import collections
 import zipfile
 
-# comments, num, fn = [], "90052", "data/channel/%s.txt"
 next = "Next nothing is (\d+)"
+
+# look through all files
+# comments, num, fn = [], "90052", "data/channel/%s.txt"
 # while True:
 #     try:
 #         with open(fn%num,"r") as f:
@@ -18,6 +20,7 @@ next = "Next nothing is (\d+)"
 # with open(fn%num,"r") as f:
 #     print f.read()
 
+# process zip file
 myzip = zipfile.ZipFile("data/channel.zip")
 comments, num, fn = [], "90052", "%s.txt"
 
@@ -25,13 +28,20 @@ while True:
     try:
         num = search(next, myzip.read(fn % num)).group(1)
     except:
-        print myzip.read(fn % num)
+        # last file, so print contents
+        # print myzip.read(fn % num)
         break
 
+    # get the comments, as instructed
+
+    # append comments
+    # comments.append(myzip.getinfo(fn % num).comment)
+
+    # oxygen did not work
+    # instead only append alphabetic, and only once
     newc=myzip.getinfo(fn % num).comment
     if newc not in comments and newc.isalpha():
         comments.append(newc)
-    # comments.append(myzip.getinfo(fn % num).comment)
 
 # print comments
 print "".join(comments)

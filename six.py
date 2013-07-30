53f93efd11c2ef8bbc14d2fe6940dfba20147687
rarch <rarcher496@gmail.com>
2013-07-30 15:55:32 -0700

    not adding data, finished six

---
+++ six.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from re import search
+import collections
+import zipfile
+
+# comments, num, fn = [], "90052", "data/channel/%s.txt"
+next = "Next nothing is (\d+)"
+# while True:
+#     try:
+#         with open(fn%num,"r") as f:
+#             # contents = f.read()
+#             num = search(next,f.read()).group(1)
+#             # num = search(next,contents).group(1)
+#     except:
+#         break
+#     # print contents
+
+# with open(fn%num,"r") as f:
+#     print f.read()
+
+myzip = zipfile.ZipFile("data/channel.zip")
+comments, num, fn = [], "90052", "%s.txt"
+
+while True:
+    try:
+        num = search(next, myzip.read(fn % num)).group(1)
+    except:
+        print myzip.read(fn % num)
+        break
+
+    comments.append(myzip.getinfo(fn % num).comment)
+# print comments
+print "".join(comments)
